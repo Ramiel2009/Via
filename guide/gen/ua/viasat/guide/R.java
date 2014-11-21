f4aeb24980d945fec65fb993149c4e572b184a19
Ramiel2009 <Ramiel2009@gmail.com>
2014-11-21 13:58:42 +0200

    Navigation Drawer added

--- guide/gen/ua/viasat/guide/R.java
+++ guide/gen/ua/viasat/guide/R.java
@@ -8,7 +8,16 @@
 package ua.viasat.guide;
 
 public final class R {
+    public static final class array {
+        public static final int views_array=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int drawer_item_color=0x7f040002;
+        public static final int list_background=0x7f040000;
+        public static final int list_background_pressed=0x7f040001;
+        public static final int list_divider=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,32 +27,44 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_drawer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int list_item_bg_normal=0x7f020002;
+        public static final int list_item_bg_pressed=0x7f020003;
+        public static final int list_selector=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnRefresh=0x7f080002;
-        public static final int lay=0x7f080000;
-        public static final int rl=0x7f080003;
-        public static final int scrollView1=0x7f080001;
-        public static final int sname=0x7f080004;
-        public static final int ssyn=0x7f080005;
+        public static final int action_settings=0x7f0a000a;
+        public static final int btnRefresh=0x7f0a0004;
+        public static final int content_frame=0x7f0a0006;
+        public static final int drawer_layout=0x7f0a0001;
+        public static final int label=0x7f0a0000;
+        public static final int lay=0x7f0a0002;
+        public static final int left_drawer=0x7f0a0007;
+        public static final int rl=0x7f0a0005;
+        public static final int scrollView1=0x7f0a0003;
+        public static final int sname=0x7f0a0008;
+        public static final int ssyn=0x7f0a0009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int synopsis=0x7f030001;
+        public static final int drawer_list_item=0x7f030000;
+        public static final int fragment_first=0x7f030001;
+        public static final int fragment_second=0x7f030002;
+        public static final int fragment_third=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int synopsis=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int menu=0x7f060002;
     }
     public static final class style {
         /** 
@@ -67,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- guide/src/ua/viasat/guide/MainActivity.java
+++ guide/src/ua/viasat/guide/MainActivity.java
@@ -5,16 +5,33 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import ua.viasat.guide.FirstFragment;
+import ua.viasat.guide.SecondFragment;
+import ua.viasat.guide.ThirdFragment;
+
+
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.opengl.Visibility;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -25,7 +42,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	public static String lchannel;
 	public static String lsynopse;
 	ProgressDialog pd; // Loading dialog
-	TextView tvd;
+	static TextView tvd;
 	String Url = "http://ru.viasat.ua/contents";
 	public static int i; // titles counter
 	public static Document doc;
@@ -36,6 +53,19 @@ public class MainActivity extends Activity implements OnClickListener {
 	public static int clickID; // get clicked title id
 	public static String[] contentUrl;
 	public static String test;
+	
+	
+	
+	 private DrawerLayout myDrawerLayout;
+	    private ListView myDrawerList;
+	    private ActionBarDrawerToggle myDrawerToggle;
+	 
+	    // navigation drawer title
+	    private CharSequence myDrawerTitle;
+	    // used to store app title
+	    private CharSequence myTitle;
+	 
+	    private String[] viewsNames;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +76,160 @@ public class MainActivity extends Activity implements OnClickListener {
 		Context dRequest = new Context();
 		dRequest.execute();
 		TVCreator();
+		
+		
+		
+		//NAVIGATION DRAWER
+		
+		myTitle =  getTitle();	
+        myDrawerTitle = getResources().getString(R.string.menu);
+ 
+        // load slide menu items             
+        viewsNames = getResources().getStringArray(R.array.views_array);
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        myDrawerList = (ListView) findViewById(R.id.left_drawer);
+ 
+        myDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, viewsNames));
+ 
+        // enabling action bar app icon and behaving it as toggle button
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);        
+ 
+        myDrawerToggle = new ActionBarDrawerToggle(this, myDrawerLayout,
+            R.drawable.ic_drawer, //nav menu toggle icon
+            R.string.app_name, // nav drawer open - description for accessibility
+            R.string.app_name // nav drawer close - description for accessibility
+        ){
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(myTitle);
+                // calling onPrepareOptionsMenu() to show action bar icons
+                invalidateOptionsMenu();
+            }
+ 
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(myDrawerTitle);
+                // calling onPrepareOptionsMenu() to hide action bar icons
+                invalidateOptionsMenu();
+            }
+        };
+        myDrawerLayout.setDrawerListener(myDrawerToggle);
+ 
+        if (savedInstanceState == null) {
+            // on first time display view for first nav item
+            displayView(0);
+        }
+ 
+        myDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 	}
+	
+	
+	
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(
+            AdapterView<?> parent, View view, int position, long id
+        ) {
+            // display view for selected nav drawer item
+            displayView(position);
+        }               
+    }
+    
+    
+    private void displayView(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = null;
+        switch (position) {
+        case 0:
+            fragment = new FirstFragment();
+            break;
+        case 1:
+        	//tvd.setVisibility(View.GONE);
+            fragment = new SecondFragment();
+            break;
+        case 2:
+        	fragment = new ThirdFragment();
+            break;
+        default:
+            break;
+        }
+ 
+        if (fragment != null) {
+            android.app.FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment).commit();
+ 
+            // update selected item and title, then close the drawer
+            myDrawerList.setItemChecked(position, true);
+            myDrawerList.setSelection(position);
+            setTitle(viewsNames[position]);
+            myDrawerLayout.closeDrawer(myDrawerList);
+        } else {
+            // error in creating fragment
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+ 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // toggle nav drawer on selecting action bar app icon/title
+        if (myDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action bar actions click
+        switch (item.getItemId()) {
+        case R.id.action_settings:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+ 
+    /**
+     * Called when invalidateOptionsMenu() is triggered
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // if navigation drawer is opened, hide the action items
+        boolean drawerOpen = myDrawerLayout.isDrawerOpen(myDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+    
+    
+    
+    @Override
+    public void setTitle(CharSequence title) {
+        myTitle = title;
+        getActionBar().setTitle(myTitle);
+    }
+ 
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        myDrawerToggle.syncState();
+    }
+ 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        myDrawerToggle.onConfigurationChanged(newConfig);
+    }
+	
+	//NAVIGATION DRAWER 
+	
 
 	public void TVCreator() {
 		for (int a = i; a > 0; a--) {
@@ -124,4 +307,5 @@ public class MainActivity extends Activity implements OnClickListener {
 			TVCreator();
 		}
 	}
+	
 }
